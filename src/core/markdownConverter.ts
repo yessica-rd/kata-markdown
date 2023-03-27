38c155814cee9cb40d1553190cab38ea3dcba6d3
yessica-rd <yessica@leanmind.es>
2023-03-27 20:40:26 +0100

    GREEN: multiple links are converted

--- src/core/markdownConverter.ts
+++ src/core/markdownConverter.ts
@@ -1,23 +1,66 @@
 export const markdownConverter = (markdownText: string) => {
-  const anchorsRegex = /\[(.*?)\]\((.*?)\)/;
-  const markdownTextMatch = anchorsRegex.exec(markdownText);
+  const anchorsRegex = /\[(.*?)\]\((.*?)\)/g;
+  const numberOfReferences = (markdownText.match(anchorsRegex) || []).length;
   
-  if(markdownTextMatch) {
-    const linkText = `${markdownTextMatch[1]} [^anchor1]`;
-    const footer = `\n\n[^anchor1]: ${markdownTextMatch[2]}`;
-    const markdownTextBeforeLink = markdownText.substring(0, markdownTextMatch.index);
-    const markdownTextAfterLink = markdownText.substring(markdownTextMatch.index + markdownTextMatch[0].length);
-
-    if(markdownTextBeforeLink && markdownTextAfterLink) {
-      return `${markdownTextBeforeLink}${linkText}${markdownTextAfterLink}${footer}`;
-    }
-    if(markdownTextBeforeLink) {
-      return `${markdownTextBeforeLink}${linkText}${footer}`;
-    }
-    if(markdownTextAfterLink) {
-      return `${linkText}${markdownTextAfterLink}${footer}`;
-    }
-    return `${linkText}${footer}`;
+  if(!numberOfReferences) {
+    return markdownText
   }
-  return markdownText;
+  let markdownTextMatch = anchorsRegex.exec(markdownText);
+  let newText = markdownText;
+  let anchorCounter = 1;
+  const result = [];
+  const footerAnchors = [];
+
+  while (markdownTextMatch != null) {
+    const linkText = markdownTextMatch[1];
+    const anchorUrl = markdownTextMatch[2];
+
+    const startIndex = newText.indexOf(markdownTextMatch[0]);
+    const endIndex = startIndex + markdownTextMatch[0].length;
+    const anchorName = `[^anchor${anchorCounter}]`;
+    const anchorFooterUrl = `${anchorName}: ${anchorUrl}`;
+    const isLastAnchor = anchorCounter === numberOfReferences;
+    const newTextLink = `${linkText} ${anchorName}${isLastAnchor ? '' : ' '}`;
+    
+    const markdownTextBeforeLink = newText.slice(0, startIndex);
+    if(markdownTextBeforeLink) {
+      result.push(markdownTextBeforeLink)
+    }
+    result.push(newTextLink)
+    if(isLastAnchor) {
+      const markdownTextAfterLink = newText.slice(endIndex)
+      result.push(markdownTextAfterLink);
+    }
+    newText = newText.substring(endIndex)
+    footerAnchors.push(anchorFooterUrl);
+    anchorCounter ++;
+
+    markdownTextMatch = anchorsRegex.exec(markdownText);
+  }
+  return `${result.join('')}\n\n${footerAnchors.join('\n\n')}`;
+
+
+
+
+
+
+  
+  // if(markdownTextmarkdownTextMatch) {
+  //   const linkText = `${markdownTextmarkdownTextMatch[1]} [^anchor1]`;
+  //   const footer = `\n\n[^anchor1]: ${markdownTextmarkdownTextMatch[2]}`;
+  //   const markdownTextBeforeLink = markdownText.substring(0, markdownTextmarkdownTextMatch.index);
+  //   const markdownTextAfterLink = markdownText.substring(markdownTextmarkdownTextMatch.index + markdownTextmarkdownTextMatch[0].length);
+
+  //   if(markdownTextBeforeLink && markdownTextAfterLink) {
+  //     return `${markdownTextBeforeLink}${linkText}${markdownTextAfterLink}${footer}`;
+  //   }
+  //   if(markdownTextBeforeLink) {
+  //     return `${markdownTextBeforeLink}${linkText}${footer}`;
+  //   }
+  //   if(markdownTextAfterLink) {
+  //     return `${linkText}${markdownTextAfterLink}${footer}`;
+  //   }
+  //   return `${linkText}${footer}`;
+  // }
+  // return markdownText;
 }
