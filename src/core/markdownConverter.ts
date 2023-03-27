5e131a39d618af5ac72f25aec53c85635a0a46b7
yessica-rd <yessica@leanmind.es>
2023-03-27 18:17:11 +0100

    GREEN: provided link after text string creates anchor and footer after

--- src/core/markdownConverter.ts
+++ src/core/markdownConverter.ts
@@ -5,6 +5,11 @@ export const markdownConverter = (markdownText: string) => {
   if(markdownTextMatch) {
     const linkText = `${markdownTextMatch[1]} [^anchor1]`;
     const footer = `\n\n[^anchor1]: ${markdownTextMatch[2]}`;
+    const markdownTextBeforeLink = markdownText.substring(0, markdownTextMatch.index);
+
+    if(markdownTextBeforeLink) {
+      return `${markdownTextBeforeLink}${linkText}${footer}`;
+    }
     return `${linkText}${footer}`;
   }
   return markdownText;
--- src/tests/markdownConverter.test.ts
+++ src/tests/markdownConverter.test.ts
@@ -3,7 +3,7 @@ import {markdownConverter} from '../core/markdownConverter';
 // [x] - '' -> ''
 // [x] - 'Random text' -> 'Random text'
 // [x] - '[Google](https://www.google.es)' -> 'Google [^anchor1]\n\n[^anchor1]: https://www.google.es'
-// [] - 'Random text before [Google](https://www.google.es)' -> 'Random text before Google [^anchor1]\n\n[^anchor1]: https://www.google.es'
+// [x] - 'Random text before [Google](https://www.google.es)' -> 'Random text before Google [^anchor1]\n\n[^anchor1]: https://www.google.es'
 // [] - '[Google](https://www.google.es) random text after' -> 'Google [^anchor1] random text after\n\n[^anchor1]: https://www.google.es'
 // [] - 'texto random before [Google](https://www.google.es) texto random after' -> 'texto random Google [^anchor1] texto random\n\n[^anchor1]: https://www.google.es'
 // [] - '[Google](https://www.google.es)[Github](https://github.com)' -> 'Google [^anchor1] Github [^anchor2]\n\n[^anchor1]: https://www.google.es\n\n[^anchor2]: https://github.com'
@@ -23,6 +23,6 @@ describe('Markdown converter kata', () => {
   it('provided link after text string creates anchor and footer after', () => {
     const givenText = 'Random text before [Google](https://www.google.es)';
     const expectedText = 'Random text before Google [^anchor1]\n\n[^anchor1]: https://www.google.es'
-    expect(markdownConverter(givenText)).toBe(expectedText)
+    expect(markdownConverter(givenText)).toBe(expectedText);
   });
 });
