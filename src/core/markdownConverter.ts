dadafd77e726022f78402ce03d55dbdc4fe206ff
yessica-rd <yessica@leanmind.es>
2023-03-27 18:10:30 +0100

    GREEN: provided link creates anchor and footer

--- src/core/markdownConverter.ts
+++ src/core/markdownConverter.ts
@@ -1,3 +1,11 @@
 export const markdownConverter = (markdownText: string) => {
+  const anchorsRegex = /\[(.*?)\]\((.*?)\)/;
+  const markdownTextMatch = anchorsRegex.exec(markdownText);
+  
+  if(markdownTextMatch) {
+    const linkText = `${markdownTextMatch[1]} [^anchor1]`;
+    const footer = `\n\n[^anchor1]: ${markdownTextMatch[2]}`;
+    return `${linkText}${footer}`;
+  }
   return markdownText;
 }
