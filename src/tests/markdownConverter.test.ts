ece2ac75b43ddb9b7af534956d6cb1a395a4e94d
yessica-rd <yessica@leanmind.es>
2023-03-27 18:02:35 +0100

    Red: empty text is allowed

--- src/tests/markdownConverter.test.ts
+++ src/tests/markdownConverter.test.ts
@@ -1,5 +1,13 @@
-describe('Your tests', () => {
-    it('expect true to be true', () => {
-        expect(true).toBeTruthy()
-    })
+// [] - '' -> ''
+// [] - 'Random text' -> 'Random text'
+// [] - '[Google](https://www.google.es)' -> 'Google [^anchor1]\n\n[^anchor1]: https://www.google.es'
+// [] - 'Random text before [Google](https://www.google.es)' -> 'Random text before Google [^anchor1]\n\n[^anchor1]: https://www.google.es'
+// [] - '[Google](https://www.google.es) random text after' -> 'Google [^anchor1] random text after\n\n[^anchor1]: https://www.google.es'
+// [] - 'texto random before [Google](https://www.google.es) texto random after' -> 'texto random Google [^anchor1] texto random\n\n[^anchor1]: https://www.google.es'
+// [] - '[Google](https://www.google.es)[Github](https://github.com)' -> 'Google [^anchor1] Github [^anchor2]\n\n[^anchor1]: https://www.google.es\n\n[^anchor2]: https://github.com'
+// [] - '[Google](https://www.google.es), [Github](https://github.com)', 'Google [^anchor1], Github [^anchor2]\n\n[^anchor1]: https://www.google.es\n\n[^anchor2]: https://github.com']
+describe('Markdown converter kata', () => {
+  it('Red: empty text is allowed', () => {
+    expect(markdownConverter('')).toBe('')
+});
 });
